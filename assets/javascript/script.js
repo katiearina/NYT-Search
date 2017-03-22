ac2eac6ecd816e59d5eb0f635f322285cb7ac0e8
Katie <katiearina@gmail.com>
2017-03-21 21:18:40 -0400

    Changes and stuff

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,8 +1,8 @@
 var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-var search = $("#search-box").val().trim();
-var beginYearInput = $("#start-year").val().trim();
+var search = $("#search-box").val();
+var beginYearInput = $("#start-year").val();
 var beginYearSearch = beginYearInput + 0101;
-var endYearInput = $("#end-year").val().trim();
+var endYearInput = $("#end-year").val();
 var endYearSearch = endYearInput + 1231;
 var searchCount = $("#search-num").val();
 
@@ -13,6 +13,16 @@ url += '?' + $.param({
 	'end_date': endYearSearch,
 	'page': searchCount
 });
+
+// url += '?' + $.param({
+// 	'q': "Clinton",
+// 	'api-key': "aca26c04925149fa8f42f16e0ef758fb",
+// 	'begin_date': 20000101,
+// 	'end_date': 20151231,
+// 	'page': 5
+// });
+
+  console.log(url);
 $.ajax({
   url: url,
   method: 'GET',
