bdb1c019f3803659f3e2690c7e34a9d6a4b19a28
Katie <katiearina@gmail.com>
2017-03-21 20:53:22 -0400

    Updated JS code

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,13 +1,15 @@
 var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+var search = $("#search-box").val().trim();
 var beginYearInput = $("#start-year").val().trim();
 var beginYearSearch = beginYearInput + 0101;
 var endYearInput = $("#end-year").val().trim();
 var endYearSearch = endYearInput + 1231;
 
 url += '?' + $.param({
-  'api-key': "aca26c04925149fa8f42f16e0ef758fb",
-  'begin_date': "19900131",
-  'end_date': "19991231"
+	'q': search,
+	'api-key': "aca26c04925149fa8f42f16e0ef758fb",
+	'begin_date': beginYearSearch,
+	'end_date': endYearSearch
 });
 $.ajax({
   url: url,
