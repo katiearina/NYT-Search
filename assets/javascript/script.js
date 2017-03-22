5f6eaada5744235417c3b6a5dfd95ad22239cfcd
Katie <katiearina@gmail.com>
2017-03-21 20:50:17 -0400

    Added JS code for year begin & end

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,4 +1,9 @@
 var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+var beginYearInput = $("#start-year").val().trim();
+var beginYearSearch = beginYearInput + 0101;
+var endYearInput = $("#end-year").val().trim();
+var endYearSearch = endYearInput + 1231;
+
 url += '?' + $.param({
   'api-key': "aca26c04925149fa8f42f16e0ef758fb",
   'begin_date': "19900131",
@@ -9,6 +14,8 @@ $.ajax({
   method: 'GET',
 }).done(function(result) {
   console.log(result);
+
 }).fail(function(err) {
   throw err;
 });
+
