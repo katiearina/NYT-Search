8951937a50a8c0cc1756a3546715b3078ac2a805
Michael Eggers <mdeggers84@gmail.com>
2017-03-22 00:01:13 -0400

    :smiley_cat: modified ajax call

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -35,23 +35,33 @@
 
 $( document ).ready(function() {
 
-$("#search-button").on("click", function(event) {
-	event.preventDefault();
-	var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-	var search = $("#search-box").val();
-	var beginYearInput = $("#start-year").val();
-	var beginYearSearch = beginYearInput + "0101";
-	var endYearInput = $("#end-year").val();
-	var endYearSearch = endYearInput + 1231;
-	var searchCount = $("#search-num").val();
+	$("#search-button").on("click", function(event) {
+		event.preventDefault();
+		var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+		var search = $("#search-box").val();
+		var beginYearInput = $("#start-year").val();
+		var endYearInput = $("#end-year").val();
+		var searchCount = $("#search-num").val();
 
-	url += '?' + $.param({
-		'q': search,
-		'api-key': "aca26c04925149fa8f42f16e0ef758fb",
-		'begin_date': beginYearSearch,
-		'end_date': endYearSearch,
-		'page': searchCount
-	});
+		url += '?' + $.param({
+			'q': search,
+			'api-key': "aca26c04925149fa8f42f16e0ef758fb",
+			'page': searchCount
+		});
+
+		// added conditional to check if dates have been put in the text box before adding them to the url
+		if ($("#start-year").val() !== "") {
+			url += "&" + $.param({
+				'begin_date': beginYearInput + "0101"
+			});
+		}
+		if ($("#end-year").val() !== "") {
+			url += "&" + $.param({
+				'end_date': endYearInput + "1231"
+			});
+
+		}
+
 
 	// url += '?' + $.param({
 	// 	'q': "Clinton",
@@ -61,15 +71,15 @@ $("#search-button").on("click", function(event) {
 	// 	'page': 5
 	// });
 
-	  console.log(url);
+	console.log(url);
 	$.ajax({
-	  url: url,
-	  method: 'GET',
+		url: url,
+		method: 'GET',
 	}).done(function(result) {
-	  console.log(result);
-	  $("#search-results").text(JSON.stringify(result));
+		console.log(result);
+		$("#search-results").text(JSON.stringify(result));
 	}).fail(function(err) {
-	  throw err;
+		throw err;
 	});
 });
 });
